9e89da130578cef2bf79271bd16782227fb15ae0
Akshay Nagpal <akshay2626@gmail.com>
2014-12-21 00:30:44 +0530

    added more snippets

--- pythonchallenge/level1.py
+++ pythonchallenge/level1.py
@@ -1,6 +1,6 @@
 # http://www.pythonchallenge.com/pc/def/map.html
 from string import maketrans
-data = "http://www.pythonchallenge.com/pc/def/map.html"
+data = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 incoming = "abcdefghijklmnopqrstuvwxyz"
 outgoing = "cdefghijklmnopqrstuvwxyzab"
 trans = maketrans(incoming,outgoing)
